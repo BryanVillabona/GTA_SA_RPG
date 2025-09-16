1d15eae11fb2ca7f8d112636acf21111f58f1ef5
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-09-16 01:17:01 -0500

    feat: :construction: Working on classes creation
    Main class created
    
    none

---
+++ models/Personaje.js
@@ -0,0 +1,77 @@
+class Personaje {
+    constructor(id, nombre, vida, ataque, defensa, nivel, habilidadEspecial, inventario) {
+        if (this.constructor === Personaje) {
+            throw new Error("No se puede instanciar la clase abstracta 'Chismosa'")
+        }
+
+        if (id > 0 && typeof id === "number") {
+            this.id = id;
+        } else {
+            throw new Error("Error en id")
+        }
+
+        if (typeof nombre === "string" && nombre.length !== 0) {
+            this.nombre = nombre;
+        } else {
+            throw new Error ("Error en nombre")
+        }
+
+        if (typeof vida === "number" && vida >= 0) {
+            this.vida = vida;
+        } else {
+            throw new Error ("Error en vida")
+        }
+
+        if (typeof ataque === "number" && ataque >= 0) {
+            this.ataque = ataque;
+        } else {
+            throw new Error ("Error en ataque")
+        }
+
+        if (typeof defensa === "number" && defensa >= 0) {
+            this.ataque = defensa;
+        } else {
+            throw new Error ("Error en defensa")
+        }
+
+        if (typeof nivel === "number" && nivel >= 1) {
+            this.nivel = nivel;
+        } else {
+            throw new Error ("Error en nivel")
+        }
+
+        if (typeof habilidadEspecial === "object") {
+            this.habilidadEspecial = habilidadEspecial;
+        } else {
+            throw new Error ("Error en habilidadEspecial")
+        }
+
+        if (typeof inventario === "object") {
+            this.inventario = inventario;
+        } else {
+            throw new Error ("Error en inventario")
+        }
+    }
+
+    atacar() {
+        throw new Error("Método debe ser implementado en subclases posteriores");
+    }
+
+    recibirDaño(dano) {
+        const danoReal = Math.max(0, dano - this.defensa);
+        this.vida -= danoReal;
+        console.log(`${this.nombre} recibió ${danoReal} de daño. Vida restante: ${this.vida}`);
+    }
+
+    usarHabilidad(objetivo) {
+        throw new Error("Método debe ser implementado en subclases posteriores");
+    }
+
+    subirNivel() {
+        this.nivel++;
+        this.vida += 20;
+        this.defensa += 5;
+        this.ataque += 2;
+        console.log(`${this.nombre} ha subido a nivel ${this.nivel}`)
+    }
+}
