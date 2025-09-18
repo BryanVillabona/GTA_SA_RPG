08844d43f23ae384296a4e2fafe42356fabd7549
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-09-18 10:39:00 -0500

    style: :bulb: Comments
    comments on Personaje.js for better understanding
    
    none

--- models/Personaje.js
+++ models/Personaje.js
@@ -82,22 +82,42 @@ class Personaje {
     this.estado[nombreEfecto] = valor;
   }
 
-  
+  // Método para finalizar turno
   finalizarTurno(notificador = null) {
+
+    // Se itera sobre el valor del estado
     for (const efecto in this.estado) {
+
+      // Si estado no exite salta la iteración
       if (!this.estado[efecto]) continue;
 
+      // Daño por quemadura
       if (efecto === 'quemadura' && this.estado.quemadura.dañoPorTurno > 0) {
+
+        // Se obtiene el daño por quemadura por turno
         const danoQuemadura = this.estado.quemadura.dañoPorTurno;
+
+        // El valor minimo de vida se establece en 0
         this.vida = Math.max(0, this.vida - danoQuemadura);
+
+        // Si notificador está presente
         if (notificador) {
+
+          // Se actualiza por iteración el turno donde el personaje asume la quemadura
           const turnosRestantes = this.estado.quemadura.duracion - 1;
+
+          // Notificador imprime el mensaje en cada iteración
           notificador.mostrarAccion(`${this.nombre} sufre ${danoQuemadura} de daño por quemadura. (${turnosRestantes} turnos restantes)`);
         }
       }
 
+      // Verificación si estado es de tipo numero
       if (typeof this.estado[efecto].duracion === 'number') {
+
+        // Se resta la duración del efecto
         this.estado[efecto].duracion--;
+
+        // Validación para quitar el efecto del personaje afectado
         if (this.estado[efecto].duracion <= 0) {
           if (notificador && efecto !== 'quemadura') notificador.mostrarAccion(`El efecto de ${efecto} ha terminado para ${this.nombre}.`);
           delete this.estado[efecto];
@@ -106,19 +126,38 @@ class Personaje {
     }
   }
 
+  // Método para usar la habilidad
   usarHabilidad(objetivo, notificador = null) {
+
+    // Se llama al notificador para que muestre la acción de la habilidad usada
     if(notificador) notificador.mostrarAccion(`${this.nombre} usa ${this.habilidadEspecial.nombre || 'una habilidad'}.`);
+
+    // Se llama al método atacar
     this.atacar(objetivo, notificador);
   }
 
+  // Método para subir de nivel
   subirNivel() {
+
+    // El atributo nivel sube en 1
     this.nivel += 1;
+
+    // La vida maxima aumenta según lo estipulado en el archivo config.js
     this.vidaMaxima += config.NIVELES.VIDA_MAX_POR_NIVEL;
+
+    // Se asigna el valor de la vida maxima a la vida original
     this.vida = this.vidaMaxima;
+
+    // Atributo de ataque sube según lo estipulado en el archivo config.js
     this.ataque += config.NIVELES.ATAQUE_POR_NIVEL;
+
+    // Atributo de defensa sube según lo estipulado en el archivo config.js
     this.defensa += config.NIVELES.DEFENSA_POR_NIVEL;
+
+    // Atributo nivel es reasignado
     return this.nivel;
   }
 }
+
 
 module.exports = Personaje;
